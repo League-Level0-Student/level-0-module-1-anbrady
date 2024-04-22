7d4e41c6aa43ae8bdec4977b3b2bf88270e48399
abbey brady <anbrady@ucsb.com>
2024-04-22 16:48:13 -0700

    Commit 4.2

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -32,14 +32,35 @@ public class RobotInSpace extends KeyboardAdapter {
 	private void moveRobot(int keyPressed) {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
+		System.out.print(keyPressed + "\n");	
 		
 		// 3. If the up arrow is pressed, move the Robot up the screen.
+		//Up = 38
+		if (keyPressed == 38) {
+			rob.setAngle(0);
+			rob.move(25);
+		}
 		
 		// 4. If the down arrow is pressed, move the Robot down.
-		
-		// 5. If the left arrow is pressed, make the Robot go left.
+		//Down = 40
+		if (keyPressed == 40) {
+			rob.setAngle(180);
+			rob.move(25);
+		}
 		
 		// 6. If right is pressed, move the Robot right.
+		//Right = 39
+		if (keyPressed == 39) {
+			rob.setAngle(90);
+			rob.move(25);
+		}
+		
+		// 5. If the left arrow is pressed, make the Robot go left.
+		//Left=37
+		if (keyPressed == 37) {
+			rob.setAngle(270);
+			rob.move(25);
+		}
 		
 		// 7. Run your program and move the Robot to R2-D2 for a surprise!
 		
@@ -49,7 +70,7 @@ public class RobotInSpace extends KeyboardAdapter {
 		int robotLocationX = rob.getX();
 		int robotLocationY = rob.getY();
 
-		if (robotLocationX <= 7300 && robotLocationX >= 720 && robotLocationY >= 150 && robotLocationY <= 160)
+		if (robotLocationX <= 730 && robotLocationX >= 720 && robotLocationY >= 150 && robotLocationY <= 160)
 			playEureka();
 	}
 
--- src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_04_int/_3_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,6 +1,10 @@
 
 void setup() {
-
+  PImage face = loadImage("face.jpeg");
+  
+  size(500, 500);
+  face.resize(100,100);
+  image(face, 0 , 0);
 }
 void draw() {
 
